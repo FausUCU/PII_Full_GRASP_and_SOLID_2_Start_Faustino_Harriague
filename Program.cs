c6d51fcef3cb9883fcff602087d797de645d45a0
[Faustino <[faustino.harriague@correo.ucu.edu.uy]>
2021-09-07 20:26:18 -0300

    Echo

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿//-------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Universidad Católica del Uruguay">
+// Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//-------------------------------------------------------------------------
+
+using System;
+using System.Collections;
+using System.Linq;
+using Full_GRASP_And_SOLID.Library;
+
+namespace Full_GRASP_And_SOLID
+{
+    public class Program
+    {
+        private static ArrayList productCatalog = new ArrayList();
+
+        private static ArrayList equipmentCatalog = new ArrayList();
+
+        public static void Main(string[] args)
+        {
+            PrintConsole print=new PrintConsole();
+            PopulateCatalogs();
+
+            Recipe recipe = new Recipe();
+            recipe.FinalProduct = GetProduct("Café con leche");
+            recipe.AddStep(new Step(GetProduct("Café"), 100, GetEquipment("Cafetera"), 120));
+            recipe.AddStep(new Step(GetProduct("Leche"), 200, GetEquipment("Hervidor"), 60));
+            /*El cambio que realice fue crear una clase PrintConsole separa de Recepie que se encarge de imprima un sring dada en la consola,
+            pero mantuve en la clase Recpie la funcion de generar dicho extring porque en ella estan todas las funciones para determinar
+            el texto que deve ser impreso.
+            Extraer la responsa de Escrivir en la consola de la funcion Recipe e generar una clase nueva para hacerlo es
+            un aplicacion del principio SRP ya que no hay nada en Recipe que la haga apropiada para escrivir en la consola
+            y es algo que requiere su propia clase, pero que Recipe mantenga la responsavilidad de generar el texto a imprimir
+            es una aplicacion de EXPERT ya que Recipe tiene toda la informacion que deve ser impresa  */
+            print.PrinttoConsole(recipe.PrintRecipe());
+
+
+        }
+
+        private static void PopulateCatalogs()
+        {
+            AddProductToCatalog("Café", 100);
+            AddProductToCatalog("Leche", 200);
+            AddProductToCatalog("Café con leche", 300);
+
+            AddEquipmentToCatalog("Cafetera", 1000);
+            AddEquipmentToCatalog("Hervidor", 2000);
+        }
+
+        private static void AddProductToCatalog(string description, double unitCost)
+        {
+            productCatalog.Add(new Product(description, unitCost));
+        }
+
+        private static void AddEquipmentToCatalog(string description, double hourlyCost)
+        {
+            equipmentCatalog.Add(new Equipment(description, hourlyCost));
+        }
+
+        private static Product ProductAt(int index)
+        {
+            return productCatalog[index] as Product;
+        }
+
+        private static Equipment EquipmentAt(int index)
+        {
+            return equipmentCatalog[index] as Equipment;
+        }
+
+        private static Product GetProduct(string description)
+        {
+            var query = from Product product in productCatalog where product.Description == description select product;
+            return query.FirstOrDefault();
+        }
+
+        private static Equipment GetEquipment(string description)
+        {
+            var query = from Equipment equipment in equipmentCatalog where equipment.Description == description select equipment;
+            return query.FirstOrDefault();
+        }
+    }
+}
